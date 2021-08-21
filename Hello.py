df3ec8da1e4c2b381f7aad79b406602a4d62ecd4
20szha <szha@bu.edu>
2021-08-21 16:49:01 -0400

    Changes 1

--- Hello.py
+++ Hello.py
@@ -1,1 +1,28 @@
-print("BasketBall Game")
+# Import and initialize the pygame library
+import pygame
+pygame.init()
+
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    # Draw a solid blue circle in the center
+    # (screen, color - r,g,b, position on screen, size of circle)
+    pygame.draw.circle(screen, (255, 0, 0), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
